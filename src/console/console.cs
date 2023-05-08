ff78fc2ef878c5e8735e4447f6c08b6deb993481
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-05-08 21:14:49 +0200

    added basic executor logic

---
+++ src/console/console.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CLI
+{
+    // The fields and methods required for a command
+    public interface ICommand
+    {
+        string Name { get; }
+        void Execute();
+    }
+
+    // The console application
+    public class App
+    {
+        private readonly bool close = false;
+
+        private static void Execute(string command)
+        {
+            // Takes a command and tries to execute the corresponding command from the command list
+            Console.WriteLine($"Executing command: {command}");
+        }
+
+        public void Run()
+        {
+            while (!close)
+            {
+                Console.WriteLine("Please enter a command ('q' or 'quit' for exit):");
+                var result = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(result))
+                {
+                    Console.WriteLine("Empty command not allowed");
+                    continue;
+                }
+
+                if (result.Equals("q") || result.Equals("quit"))
+                {
+                    break;
+                }
+
+                Execute(result.TrimEnd().TrimStart());
+            }
+        }
+    }
+}
--- src/main.cs
+++ src/main.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
-
-public class Application
+﻿public class Application
 {
     public static void Main()
     {
-        System.Console.WriteLine("Hello World");
+        var console = new CLI.App();
+        console.Run();
     }
 }
