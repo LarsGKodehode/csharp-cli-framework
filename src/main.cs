7c759074faa343b6dd0abc8fe81cb46dc977d1e9
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-05-08 20:50:42 +0200

    initial commit

---
+++ src/main.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+public class Application
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
